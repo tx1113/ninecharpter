76dbfa78946dd998ce22c7eaaad0473df711edbb
Xu Tian <tianxustartup@gmail.com>
2014-11-13 00:30:14 -0800

    add week2 assignment

--- src/week2/FindPeak.java
+++ src/week2/FindPeak.java
@@ -7,7 +7,6 @@ public class FindPeak {
 		test();
 	}
 	
-	
     /*
      * There is an integer array which has the following features:
      * The numbers in adjacent positions are different.
@@ -65,6 +64,60 @@ public class FindPeak {
 		int rev = findPeak(A);
 		System.out.println("rev = " + rev);
 	}
+	
+	//http://www.geeksforgeeks.org/find-a-peak-in-a-given-array/
+	//follow up geeksforgeeks
+	/*
+	 * Given an array of integers. Find a peak element in it. 
+	 * An array element is peak if it is NOT smaller than its neighbors. 
+	 * For corner elements, we need to consider only one neighbor. 
+	 * For example, for input array {5, 10, 20, 15}, 20 is the only peak element. 
+	 * For input array {10, 20, 15, 2, 23, 90, 67}, there are two peak elements: 20 and 90. 
+	 * Note that we need to return any one peak element.
+	 * Following corner cases give better idea about the problem.
+	 * 1) If input array is sorted in strictly increasing order, 
+	 * the last element is always a peak element. 
+	 * For example, 50 is peak element in {10, 20, 30, 40, 50}.
+	 * 2) If input array is sorted in strictly decreasing order, 
+	 * the first element is always a peak element. 
+	 * 100 is the peak element in {100, 80, 60, 50, 20}.
+	 * 3) If all elements of input array are same, every element is a peak element.
+	 * It is clear from above examples 
+	 * that there is always a peak element in input array in any input array.
+	 */
+	
+	/*
+	 * Idea:
+	 * We can use Divide and Conquer to find a peak in O(Logn) time. 
+	 * The idea is Binary Search based, we compare middle element with its neighbors. 
+	 * If middle element is greater than both of its neighbors, then we return it. 
+	 * If the middle element is smaller than the its left neighbor, 
+	 * 	then there is always a peak in left half (Why? take few examples). 
+	 * If the middle element is smaller than the its right neighbor, 
+	 * 	then there is always a peak in right half (due to same reason as left half). 
+	 */
+	
+	//in this code, return the element rather than the index
+	//recursion version
+	public static int findPeakHelper(int[] A, int start, int end){
+		int len = A.length;
+		int mid = start + (end - start);
+		
+		if((mid == 0 || A[mid] > A[mid - 1])
+				&& (mid ==len - 1 && A[mid] > A[mid + 1])){
+			return mid;
+		}else if( mid > 0 && A[mid] < A[mid - 1]){
+			//there must exist a peak in the left side
+			return findPeakHelper(A, start, mid - 1);
+		}else
+			return findPeakHelper(A, mid + 1, end);
+	}
+	
+	public static int findPeak2(int[] A){
+		return findPeakHelper(A, 0, A.length - 1);
+	}
+	
+	
     
 	
 
